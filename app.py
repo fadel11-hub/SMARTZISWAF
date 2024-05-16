d577c2ca2b9ee2f2ae06275c40d9a51ea3f74716
Fadel <fadelamili11@gmail.com>
2024-05-16 14:50:59 +0700

    penggantian nama File Main -> Index.html

--- app.py
+++ app.py
@@ -4,26 +4,31 @@ from flask import Flask, render_template, request
 # download nltk
 preparation()
 
-#Start Chatbot
+# Start Chatbot
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return render_template("Main.html")
+    return render_template("index.html")
+
 
 @app.route("/Artikel")
 def artikel():
     return render_template("Artikel.html")
 
+
 @app.route("/About")
 def bio():
     return render_template("About.html")
+
 
 @app.route("/get")
 def get_bot_response():
     user_input = str(request.args.get('msg'))
     result = generate_response(user_input)
     return result
+
 
 if __name__ == "__main__":
     app.run(debug=True)
